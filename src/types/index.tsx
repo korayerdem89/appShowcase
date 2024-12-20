aac80459b9a662c3859537b218725b9b17d81443
korayerdem89 <developerkoray@gmail.com>
2024-12-20 19:48:39 +0300

    project build has taken

---
+++ src/types/index.tsx
@@ -0,0 +1,83 @@
+export interface TypographyProps {
+  placeholder?: undefined;
+  onPointerEnterCapture?: undefined;
+  onPointerLeaveCapture?: undefined;
+}
+
+export interface CardProps {
+  placeholder?: undefined;
+  onPointerEnterCapture?: undefined;
+  onPointerLeaveCapture?: undefined;
+}
+
+export interface CardBodyProps {
+  placeholder?: undefined;
+  onPointerEnterCapture?: undefined;
+  onPointerLeaveCapture?: undefined;
+}
+
+export interface ButtonProps {
+  placeholder?: undefined;
+  onPointerEnterCapture?: undefined;
+  onPointerLeaveCapture?: undefined;
+}
+
+export interface IconButtonProps {
+  placeholder?: undefined;
+  onPointerEnterCapture?: undefined;
+  onPointerLeaveCapture?: undefined;
+}
+
+export interface NavbarProps {
+  placeholder?: undefined;
+  onPointerEnterCapture?: undefined;
+  onPointerLeaveCapture?: undefined;
+}
+export interface AvatarProps {
+  placeholder?: undefined;
+  onPointerEnterCapture?: undefined;
+  onPointerLeaveCapture?: undefined;
+}
+
+declare module "@material-tailwind/react" {
+  interface TypographyProps {
+    placeholder?: undefined;
+    onPointerEnterCapture?: undefined;
+    onPointerLeaveCapture?: undefined;
+  }
+
+  interface CardProps {
+    placeholder?: undefined;
+    onPointerEnterCapture?: undefined;
+    onPointerLeaveCapture?: undefined;
+  }
+
+  interface CardBodyProps {
+    placeholder?: undefined;
+    onPointerEnterCapture?: undefined;
+    onPointerLeaveCapture?: undefined;
+  }
+
+  interface ButtonProps {
+    placeholder?: undefined;
+    onPointerEnterCapture?: undefined;
+    onPointerLeaveCapture?: undefined;
+  }
+
+  interface IconButtonProps {
+    placeholder?: undefined;
+    onPointerEnterCapture?: undefined;
+    onPointerLeaveCapture?: undefined;
+  }
+
+  interface NavbarProps {
+    placeholder?: undefined;
+    onPointerEnterCapture?: undefined;
+    onPointerLeaveCapture?: undefined;
+  }
+  interface AvatarProps {
+    placeholder?: undefined;
+    onPointerEnterCapture?: undefined;
+    onPointerLeaveCapture?: undefined;
+  }
+}
