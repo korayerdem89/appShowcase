b65ec032594b78cd69ab0641b70c52ffc09f88df
korayerdem89 <developerkoray@gmail.com>
2024-12-20 21:15:51 +0300

    navbar removed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,6 +7,7 @@ import {
   Button,
 } from "@material-tailwind/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 interface NavItemProps {
   children: React.ReactNode;
@@ -66,28 +67,17 @@ export function Navbar() {
       className="fixed top-0 z-50 border-0"
     >
       <div className="container mx-auto flex items-center justify-between">
-        <Typography
-          as="a"
-          href="https://www.material-tailwind.com"
-          target="_blank"
-          variant="h6"
-          color={isScrolling ? "gray" : "white"}
-        >
-          Material Tailwind
-        </Typography>
-        <ul
-          className={`ml-10 hidden items-center gap-6 lg:flex ${
-            isScrolling ? "text-gray-900" : "text-white"
-          }`}
-        >
-          <NavItem>Home</NavItem>
-          <NavItem>About Us</NavItem>
-          <NavItem>Contact Us</NavItem>
-          <NavItem href="https://www.material-tailwind.com/docs/react/installation">
-            Docs
-          </NavItem>
-        </ul>
-        <div className="hidden gap-2 lg:flex lg:items-center">
+        <a href="/" className="flex items-center">
+          <Image
+            src="/logos/logo-yourpeak.png"
+            alt="YourPeak Logo"
+            width={240}
+            height={60}
+            priority
+          />
+        </a>
+
+        {/* <div className="hidden gap-2 lg:flex lg:items-center">
           <IconButton
             variant="text"
             color={isScrolling ? "gray" : "white"}
@@ -109,13 +99,8 @@ export function Navbar() {
           >
             <i className="fa-brands fa-instagram text-base" />
           </IconButton>
-          <a href="https://www.material-tailwind.com/blocks" target="_blank">
-            <Button color={isScrolling ? "gray" : "white"} size="sm">
-              Blocks
-            </Button>
-          </a>
-        </div>
-        <IconButton
+        </div> */}
+        {/* <IconButton
           variant="text"
           color={isScrolling ? "gray" : "white"}
           onClick={handleOpen}
@@ -126,13 +111,12 @@ export function Navbar() {
           ) : (
             <Bars3Icon strokeWidth={2} className="h-6 w-6" />
           )}
-        </IconButton>
+        </IconButton> */}
       </div>
-      <Collapse open={open}>
+      {/* <Collapse open={open}>
         <div className="container mx-auto mt-4 rounded-lg border-t border-blue-gray-50 bg-white px-6 py-5">
           <ul className="flex flex-col gap-4 text-blue-gray-900">
             <NavItem>Home</NavItem>
-            <NavItem>About Us</NavItem>
             <NavItem>Contact Us</NavItem>
             <NavItem href="https://www.material-tailwind.com/docs/react/installation">
               Docs
@@ -148,14 +132,9 @@ export function Navbar() {
             <IconButton variant="text" color="gray" size="sm">
               <i className="fa-brands fa-instagram text-base" />
             </IconButton>
-            <a href="https://www.material-tailwind.com/blocks" target="_blank">
-              <Button color="gray" size="sm" className="ml-auto">
-                Blocks
-              </Button>
-            </a>
           </div>
         </div>
-      </Collapse>
+      </Collapse> */}
     </MTNavbar>
   );
 }
